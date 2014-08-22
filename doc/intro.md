1254ffd812299a541a81c70e847678c29d36facd
Pat Thomas <patthomassoftware@gmail.com>
2014-08-21 22:33:16 -0400

    Initial commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to beathacker
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/beathacker/core_test.clj
@@ -0,0 +1,7 @@
+(ns beathacker.core-test
+  (:require [clojure.test :refer :all]
+            [beathacker.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
