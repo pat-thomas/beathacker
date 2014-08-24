04260b57c696226f2dd16d9d9e8ff797234b21f0
Pat Thomas <pthomas@relaynetwork.com>
2014-08-23 20:46:52 -0400

    A second subscriber for logging.

--- src/beathacker/events.clj
+++ src/beathacker/events.clj
@@ -5,25 +5,41 @@
 
 (defchan publisher)
 (defchan subscriber)
+(defchan log-subscriber)
 
 (defn fire-event!
   [topic data]
   (go (>! publisher {:topic topic
                      :data  data})))
 
-(defn take-from-channel
-  [channel prefix]
+(defn take-event-from-channel
+  [channel]
   (async/go-loop []
     (let [val-from-chan (<! channel)]
       (do (handlers/handle-event val-from-chan)
           (recur)))))
 
-(defn init!
+(defn take-event-from-channel-for-logging
+  [channel]
+  (async/go-loop []
+    (let [val-from-chan (<! channel)]
+      (do (println "event observed:" val-from-chan)
+          (recur)))))
+
+(defn init-subscribers!
   []
   (let [publication (pub publisher #(:topic %))]
     (do (sub publication :event-fired subscriber)
-        (take-from-channel subscriber "subscriber"))))
+        (sub publication :event-fired log-subscriber)
+        :subscribers-initialized)))
 
+(defn init-event-listeners!
+  []
+  (do (take-event-from-channel subscriber)
+      (take-event-from-channel-for-logging log-subscriber)
+      :event-listeners-initialized))
 
-
-
+(defn init!
+  []
+  (do (init-subscribers!)
+      (init-event-listeners!)))
--- src/beathacker/handlers.clj
+++ src/beathacker/handlers.clj
@@ -18,5 +18,5 @@
     (handler data)))
 
 (comment
-  
+
 )
