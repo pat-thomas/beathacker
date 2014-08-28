e8b7dbb03ede3b0c2124ee20706c89f2e1c13d72
Pat Thomas <patthomassoftware@gmail.com>
2014-08-27 23:10:21 -0400

    Main app-loop fn kind of working now.

--- src/beathacker/app_loop.clj
+++ src/beathacker/app_loop.clj
@@ -5,6 +5,31 @@
 
 (defchan event-channel)
 
-(defn run-app-loop
-  []
+(def metro (overtone/metronome 120))
+
+(def debug-atom (atom [])) ;; make sure to delete this at some point
+
+(defn fire-event!
+  [msg]
+  (go (>! event-channel msg)))
+
+(defn handler
+  [channel]
+  (println "calling handler")
+  (go (when-let [val-from-channel (<! channel)]
+        (swap! debug-atom conj val-from-channel)))
   :implement-me)
+
+(defn run-app-loop
+  [nome cb channel]
+  (let [beat (nome)]
+    (overtone/at (nome beat)
+                 (cb channel))
+    (overtone/apply-by (nome (inc beat)) #'run-app-loop nome [])))
+
+(comment
+  (run-app-loop metro
+                handler
+                event-channel)
+  (fire-event! :test-event-2)
+  )
