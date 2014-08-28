a4c6e772b12d032ec95b7a78a93f0853ea92be0b
Pat Thomas <patthomassoftware@gmail.com>
2014-08-27 22:50:25 -0400

    Require overtone.live in app-loop namespace.

--- src/beathacker/app_loop.clj
+++ src/beathacker/app_loop.clj
@@ -1,6 +1,7 @@
 (ns beathacker.app-loop
-  (:require [clojure.core.async :as async :refer [go <! >!]]
-            [beathacker.util :refer [defchan]]))
+  (:require [overtone.live      :as overtone]   
+            [clojure.core.async :as async :refer [go <! >!]]
+            [beathacker.util    :refer [defchan]]))
 
 (defchan event-channel)
 
