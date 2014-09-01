d04231d6e1b8448791aea80bb64da962a2798764
Pat Thomas <pthomas@relaynetwork.com>
2014-09-01 11:43:38 -0400

    memento dependency.

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main beathacker.core/main
   :dependencies [[org.clojure/clojure     "1.6.0"]
+                 [memento                 "0.1.0"]
                  [overtone                "0.9.1"]
                  [org.clojure/tools.nrepl "0.2.3"]
                  [org.clojure/core.async  "0.1.338.0-5c5012-alpha"]])
