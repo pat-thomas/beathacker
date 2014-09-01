62d01c0fb1ea666dd576a8fb4ea21e13ac3e794f
Pat Thomas <pthomas@relaynetwork.com>
2014-09-01 11:54:17 -0400

    defchan supports arbitrary buffer expression.

--- src/beathacker/util.clj
+++ src/beathacker/util.clj
@@ -2,7 +2,7 @@
   (:require [clojure.core.async :as async]))
 
 (defmacro defchan
-  [channel-name & [buffer-size]]
-  (if buffer-size
-    `(def ~channel-name (async/chan ~buffer-size))
+  [channel-name & [buffer-expr]]
+  (if buffer-expr
+    `(def ~channel-name (async/chan ~buffer-expr))
     `(def ~channel-name (async/chan))))
