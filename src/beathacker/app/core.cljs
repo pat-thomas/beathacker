5ccd220fdc6a08630e14d6a8c1d7073aeb89133a
Pat Thomas <pthomas@relaynetwork.com>
2014-09-02 17:32:49 -0400

    XHR utils.

--- src/beathacker/app/core.cljs
+++ src/beathacker/app/core.cljs
@@ -1,12 +1,34 @@
 (ns beathacker.app.core
   (:require [goog.dom     :as gdom]
+            [goog.events  :as events]
             [om.core      :as om   :include-macros true]
             [om.core      :as dom  :include-macros true]
-            [sablono.core :as html :include-macros true]))
+            [sablono.core :as html :include-macros true])
+  (:import [goog.net XhrIo]
+           goog.net.EventType
+           [goog.events EventType]))
 
 (enable-console-print!)
 
 (def app-state (atom {:initialized false}))
+
+(def ^:private meths
+  {:get    "GET"
+   :put    "PUT"
+   :post   "POST"
+   :delete "DELETE"})
+
+(defn json-xhr
+  [{:keys [method url data on-complete]}]
+  (let [xhr (XhrIo.)]
+    (events/listen xhr goog.net.EventType.COMPLETE
+                   (fn [e]
+                     (on-complete (fn [msg]
+                                    (println msg)))))
+    (. xhr
+       (send url (meths method) (when data
+                                  (pr-str data))
+             #js {"Content-Type" "application/json"}))))
 
 (defn beathacker-app
   [data owner opts]
