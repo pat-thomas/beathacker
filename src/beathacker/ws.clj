9c8a6a56a283da3ea968d2444020be7b9ae53ba9
Pat Thomas <pthomas@relaynetwork.com>
2014-09-02 09:22:20 -0400

    Add dummy handler (not implemented yet) to handle core api post.

--- src/beathacker/ws.clj
+++ src/beathacker/ws.clj
@@ -4,14 +4,19 @@
             [compojure.handler  :as handler]
             [compojure.route    :as route]))
 
+(defn handle-core-api-req
+  [{:keys [params] :as req}]
+  "implement-me\n")
+
 (compojure/defroutes app-routes
-  (compojure/GET "/ping" [] "pong\n"))
+  (compojure/GET  "/ping" [] "pong\n")
+  (compojure/POST "/core" [] #'handle-core-api-req))
 
 (defn app
   []
   (http/run-server (handler/site #'app-routes) {:port 8080}))
 
 (comment
-(app)
+  (app)
 )
 
