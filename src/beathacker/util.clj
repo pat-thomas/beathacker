ed183cf1ddc901d03fb88e761d66e32e38df6908
Pat Thomas <patthomassoftware@gmail.com>
2014-09-01 21:42:30 -0400

    Helper fn: expr->chan+buffer to make defchan macro more readable.

--- src/beathacker/util.clj
+++ src/beathacker/util.clj
@@ -6,17 +6,21 @@
    :dropping-buffer clojure.core.async/dropping-buffer
    :sliding-buffer  clojure.core.async/sliding-buffer})
 
+(defn expr->chan+buffer
+	[buffer-type buffer-size]
+  (if-let [buffer (get buffer-type->buffer buffer-type)]
+    (list buffer buffer-size)
+    (throw (java.lang.Exception. (format "Error: %s is not a valid core.async buffer type. Valid buffer types are: %s"
+                                         (name buffer-type)
+                                         (->> buffer-type->buffer
+                                              keys
+                                              sort
+                                              (mapv name)
+                                              (mapv symbol)))))))
+
 (defmacro defchan
   [channel-name & [buffer-type buffer-size]]
-  (if (and buffer-type buffer-size)
-    (if-let [buffer (get buffer-type->buffer buffer-type)]
-      (let [buffer-expr (list buffer buffer-size)]
-       `(def ~channel-name (async/chan ~buffer-expr)))
-      (throw (java.lang.Exception. (format "Error: %s is not a valid core.async buffer type. Valid buffer types are: %s"
-                                           (name buffer-type)
-                                           (->> buffer-type->buffer
-                                                keys
-                                                sort
-                                                (mapv name)
-                                                (mapv symbol))))))
-    `(def ~channel-name (async/chan))))
+  (if-not (and buffer-type buffer-size)
+    `(def ~channel-name (async/chan))
+    `(def ~channel-name (async/chan ~(expr->chan+buffer buffer-type buffer-size)))))
+
