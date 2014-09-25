f2343d5450391f54b90d3bc828820a2f7143b2a9
Pat Thomas <pthomas@relaynetwork.com>
2014-09-24 23:59:00 -0400

    Stub out reflection component.

--- src/beathacker/app/components/rhythm_box.cljs
+++ src/beathacker/app/components/rhythm_box.cljs
@@ -23,23 +23,41 @@
                    (mapv #(om/build rhythm-box-column data)
                          (range (get-in data [:selected-option :columns])))))))
 
+(defcomponent rhythm-box-reflection-column
+  (render
+   (dom/div #js {:className "rhythm-box-column"}
+            (om/build rhythm-box data))))
+
+(defcomponent rhythm-box-reflection-row
+  (render
+   (dom/div #js {:className "rhythm-box-row reflection"}
+            (apply dom/div nil
+                   (mapv #(om/build rhythm-box-reflection-column data)
+                         (range (get-in data [:selected-option :columns])))))))
+
+(defcomponent column-select-list
+  (render
+   (dom/div nil
+            "Columns"
+            (dom/select #js {:onChange (fn [e]
+                                         (om/update! data [:selected-option :columns] (helpers/evt->value e)))}
+                        (mapv build-option (map str (range 1 9)))))))
+
+(defcomponent row-select-list
+  (render
+   (dom/div nil
+            "Rows"
+            (dom/select #js {:onChange (fn [e]
+                                         (om/update! data [:selected-option :rows] (helpers/evt->value e)))}
+                        (mapv build-option (map str (range 1 9)))))))
+
 (defcomponent rhythm-box-container
   (render
-   (dom/div
-    #js {:id "rhythm-box-container"}
-    (dom/div nil
-             "Rows"
-            (dom/select #js {:onChange (fn [e]
-                                          (om/update! data [:selected-option :rows] (helpers/evt->value e)))}
-                         (mapv build-option (map str (range 1 9)))))
-    (dom/div nil
-             "Columns"
-             (dom/select #js {:onChange (fn [e]
-                                          (om/update! data [:selected-option :columns] (helpers/evt->value e)))}
-                         (mapv build-option (map str (range 1 9)))))
-    (apply dom/div nil
-           (mapv #(om/build rhythm-box-row data)
-                 (range (get-in data [:selected-option :rows])))))))
-
-
-
+   (dom/div #js {:id "rhythm-box-container"}
+            (dom/div #js {:id "select-list-container"}
+                     (om/build row-select-list data)
+                     (om/build column-select-list data))
+            (dom/div #js {:id "rhythm-box-main"}
+                     (apply dom/div nil (mapv #(om/build rhythm-box-row data) (range (get-in data [:selected-option :rows])))))
+            #_(dom/div #js {:id "rhythm-box-reflection"}
+                     (apply dom/div nil (mapv #(om/build rhythm-box-reflection-row data) (range (get-in data [:selected-option :rows]))))))))
