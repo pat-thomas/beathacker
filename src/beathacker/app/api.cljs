e5eef00b18c7000046d45306977ef9022fccd576
Pat Thomas <patthomassoftware@gmail.com>
2014-09-27 17:24:53 -0400

    Remove unused xhr fns.

--- src/beathacker/app/api.cljs
+++ src/beathacker/app/api.cljs
@@ -2,29 +2,6 @@
   (:require [beathacker.app.xhr :refer [do-xhr]]
             [beathacker.app.models.rhythm-grid :as rhythm-grid]))
 
-(defn handle-click
-  [e]
-  (do-xhr {:method      :post
-           :url         "core"
-           :data        {:handler-name :sin
-                         :data         {:type       :note
-                                        :sound-type :sin}}
-           :on-complete (fn [res]
-                          (do (println :wat)
-                              (println res)))}))
-
-(defn handle-click-again
-  [e]
-  (do-xhr {:method      :post
-           :url         "core"
-           :data        {:handler-name :kick
-                         :data         {:type       :note
-                                        :sound-type :kick
-                                        :wait       2}}
-           :on-complete (fn [res]
-                          (do (println :huh)
-                              (println res)))}))
-
 (defn send-rhythm-to-server
   [data]
   (let [base-data {:rhythm-data (rhythm-grid/clicked-data->rhythm-data (:clicked data))
